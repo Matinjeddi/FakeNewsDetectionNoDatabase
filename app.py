91f1f5ff41b9d5dd445d7dcecf101d5b1bc17ec5
Matinjeddi <matinjeddi@hotmail.com>
2025-07-05 08:35:34 +0200

    changing port to 8000

--- app.py
+++ app.py
@@ -146,4 +146,5 @@ if __name__ == '__main__':
     # Register cleanup function
     atexit.register(lambda: print('Cleaning up...'))
     
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
